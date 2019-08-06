061102bac900b25fd2db47384527aead7883f097
Vikram Jakhar <vikram.jakhar@tothenew.com>
2019-08-07 01:00:24 +0530

    usage added

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/vikramjakhr/hiking-simulator/parser"
 	"github.com/vikramjakhr/hiking-simulator/simulator"
 	"log"
+	"os"
 )
 
+const usage = `hiking-simulator, finding shortest time for crossing a barrier.
+
+Usage:
+
+  hiking-simulator
+`
+
+func usageExit(rc int) {
+	fmt.Println(usage)
+	os.Exit(rc)
+}
+
 func main() {
+	flag.Usage = func() { usageExit(0) }
+	flag.Parse()
+
 	// Read simulation input
 	simulatorInput := parser.NewSimulationInput()
 	err := simulatorInput.LoadSimulationInput()
